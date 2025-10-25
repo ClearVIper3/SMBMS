35272ec8edb8b0c931194387d9a9abaf773d36d2
ClearVIper3 <3088663751@qq.com>
2025-10-25 16:11:36 +0800

    chore: 项目清理 - 移除IDE配置、SVN残留和编译输出

--- src/main/java/com/viper/SpringbootApplication.java
+++ src/main/java/com/viper/SpringbootApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class springbootApplication {
+public class SpringbootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(springbootApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 }
